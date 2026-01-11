3de40dd3774513776ba385f3ad1307c2234be5ef
Takahiro Yano <speg03@gmail.com>
2026-01-11 16:14:15 +0900

    npm run lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,26 @@
 import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    // eslint-plugin-react の設定
+    ...pluginReact.configs.flat.recommended, // 推奨設定をベースにする
+    ...pluginReact.configs.flat["jsx-runtime"], // "plugin:react/jsx-runtime" に相当する設定
+  },
+  {
+    ignores: ["dist/"],
+  }
 ]);
